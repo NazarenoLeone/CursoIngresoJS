26c4d2c610c2ab7ad74a7b910adce8b951bfc451
unknown <leonenazareno@gmail.com>
2022-01-29 12:22:49 -0300

    Nuevo Test

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla");
+	alert("Mi nombre es Nazareno y esto funciona de maravilla");
 }
 
